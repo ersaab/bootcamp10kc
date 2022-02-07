0bbfebec396f56f5b7604f9ab383fdf538ef53de
Lazy Er <er.harmanjitsingh@gmail.com>
2022-02-07 04:50:13 -0500

    ui part done

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -5,11 +5,13 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss']
+  styleUrls: ['./user-details.component.scss', '../user-enroll/user-enroll.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
 
   userDetailsForm: FormGroup;
+  imageUrl: any = '';
+
 
   get fc() {
     return this.userDetailsForm.controls
@@ -24,13 +26,36 @@ export class UserDetailsComponent implements OnInit {
     this.router.navigate(['']);
   }
 
-  ngOnInit(): void {
+  showPreview(event: any) {
+    const file = (event.target as HTMLInputElement).files[0];
 
+    //File Preview
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageUrl = reader.result as string;
+      this.fc['imgBase64'].patchValue(this.imageUrl);
+      // this.postData(this.mortgageApplication, () => {
+      //   //this.router.navigate(['']);
+      //   //console.log("Img Url in Preview", this.imageUrlBack);
+      //   this.spinWheel = false;
+      // });
+    }
+    reader.readAsDataURL(file);
+  }
+
+  initForm() {
     this.userDetailsForm = this.fb.group({
+      imgName: ["", [Validators.required]],
+      imgBase64: ["", [Validators.required]],
       firstName: ["", [Validators.required]],
       lastName: ["", [Validators.required]],
       email: ["", [Validators.required]],
       phoneNumber: ["", [Validators.required, Validators.maxLength(14)]],
     });
+  }
+
+
+  ngOnInit(): void {
+    this.initForm();
   }
 }
--- src/app/user-enroll/user-enroll.component.ts
+++ src/app/user-enroll/user-enroll.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-enroll',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEnrollComponent implements OnInit {
 
-  constructor() { }
+  userAddForm: FormGroup;
+  imageUrl: any = '';
 
-  ngOnInit(): void {
+  get fc() {
+    return this.userAddForm.controls
   }
 
+  constructor(public fb: FormBuilder,
+    private router: Router
+  ) {
+  }
+
+  login() {
+    this.router.navigate(['']);
+  }
+
+  showPreview(event: any) {
+    const file = (event.target as HTMLInputElement).files[0];
+
+    //File Preview
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imageUrl = reader.result as string;
+      this.fc['imgBase64'].patchValue(this.imageUrl);
+      // this.postData(this.mortgageApplication, () => {
+      //   //this.router.navigate(['']);
+      //   //console.log("Img Url in Preview", this.imageUrlBack);
+      //   this.spinWheel = false;
+      // });
+    }
+    reader.readAsDataURL(file);
+  }
+
+  initForm() {
+    this.userAddForm = this.fb.group({
+      imgName: ["", [Validators.required]],
+      imgBase64: ["", [Validators.required]],
+      firstName: ["", [Validators.required]],
+      lastName: ["", [Validators.required]],
+      email: ["", [Validators.required]],
+      phoneNumber: ["", [Validators.required, Validators.maxLength(14)]],
+      password: ["", [Validators.required, Validators.maxLength(14)]],
+    });
+  }
+
+
+  ngOnInit(): void {
+    this.initForm();
+  }
 }
